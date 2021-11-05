2b1e75a29c4fa01439a68395f3241df9aca0ef08
MtFogitu <92184365+MtFogitu@users.noreply.github.com>
2021-11-05 16:22:42 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,12 @@
+def rot(num):
+    num_list = list(str(num))
+    for i in range(len(num_list) + 1):
+        if num_list[i] == '6':
+            num_list.insert(i, '9')
+            num_list.pop(i + 1)
+            break
+        elif '6' not in num_list:
+            return num
+    return int("".join([str(l) for l in num_list]))
+if __name__ == "__main__":
+    print(rot(9999))
